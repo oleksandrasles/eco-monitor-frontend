2bacd63d25c85c2ec2d4e2109b09d81b74d54d4b
oleksandrasles <aleksandra.sles22@gmail.com>
2024-04-02 18:57:20 +0300

    commit

---
+++ functions/Type.js
@@ -0,0 +1,79 @@
+export async function getTypes() {
+  try {
+    const response = await fetch('http://localhost:4444/types', {
+      headers: {
+        accept: 'application/json',
+        'User-agent': 'eco-monitor',
+      },
+    });
+    const types = await response.json();
+    return types;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function getType(typeId) {
+  try {
+    const response = await fetch(`http://localhost:4444/types/${typeId}`, {
+      headers: {
+        accept: 'application/json',
+        'User-agent': 'eco-monitor',
+      },
+    });
+    const type = await response.json();
+    return type;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function postType(data) {
+  try {
+    const response = await fetch(`http://localhost:4444/types/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'User-agent': 'eco-monitor',
+      },
+      body: JSON.stringify(data),
+    });
+    const type = await response.json();
+    return type;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function updateType(data, typeId) {
+  try {
+    const response = await fetch(`http://localhost:4444/types/${typeId}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        'User-agent': 'eco-monitor',
+      },
+      body: JSON.stringify(data),
+    });
+    const type = await response.json();
+    return type;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function deleteType(typeId) {
+  try {
+    const response = await fetch(`http://localhost:4444/types/${typeId}`, {
+      method: 'DELETE',
+      headers: {
+        accept: 'application/json',
+        'User-agent': 'eco-monitor',
+      },
+    });
+    const type = await response.json();
+    return type;
+  } catch (error) {
+    console.error(error);
+  }
+}
